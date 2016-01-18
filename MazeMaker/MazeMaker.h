136b3c62db6086e856fea65871372fa083ab8d25
Jamis Buck <jamisbuck@gmail.com>
2016-01-17 18:18:01 -0700

    Initial Commit

---
+++ MazeMaker/MazeMaker.h
@@ -0,0 +1,19 @@
+//
+//  MazeMaker.h
+//  MazeMaker
+//
+//  Created by Jamis Buck on 1/17/16.
+//  Copyright © 2016 Jamis Buck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MazeMaker.
+FOUNDATION_EXPORT double MazeMakerVersionNumber;
+
+//! Project version string for MazeMaker.
+FOUNDATION_EXPORT const unsigned char MazeMakerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MazeMaker/PublicHeader.h>
+
+
